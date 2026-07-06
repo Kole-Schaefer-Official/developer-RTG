8d39c40db3dbe20f18efaf43bd7db0af40ab309a
Kole Schaefer <duckup0305@gmail.com>
2026-07-06 18:16:20 -0400

    COmpleted Day One

---
+++ day_one.py
@@ -0,0 +1,16 @@
+# Day One 
+
+name = input("What is your name?")
+
+print(f"Hello, {name}!")
+
+age = input("How old are you?")
+
+print(f"You are {age} years old.")
+
+occupation = input("What is your dream job?")
+
+print(f"I hope you become a {occupation}!")
+
+print("This is just the beginning!")
+
