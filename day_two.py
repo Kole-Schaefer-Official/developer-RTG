6b7c40b3e6510cacbe4ecab9fd4b1f7e03b0ce3b
Kole Schaefer <duckup0305@gmail.com>
2026-07-07 01:35:37 -0400

    Complete Mission 002

---
+++ day_two.py
@@ -0,0 +1,13 @@
+# Day Two
+
+first_number = int(input("Enter your first number: "))
+
+second_number = int(input("Enter your second number: "))
+
+print(f"The sum is {first_number + second_number}")
+
+print(f"The difference between your numbers is {first_number - second_number}")
+
+print(f"The product of your two numbers is {first_number * second_number}")
+
+print(f"The quotient of your two numbers is {first_number / second_number}")
