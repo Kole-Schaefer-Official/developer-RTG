e75693da0d479faad26287116db6939a573841dd
Kole Schaefer <duckup0305@gmail.com>
2026-07-08 01:42:27 -0400

    day_three.py

---
+++ day_three.py
@@ -0,0 +1,10 @@
+# Day Three
+
+password = input("Enter the password: ")
+
+if password == "python123":
+    print("Welcome Back!")
+    print("Access Granted.")
+else:
+    print("Incorrect password.")
+    print("Access Denied.")
