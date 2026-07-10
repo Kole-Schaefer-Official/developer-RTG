d359a72fb70cd8dd144552c0a13a966c2ea33d47
Kole Schaefer <duckup0305@gmail.com>
2026-07-09 22:56:51 -0400

    Day Four Complete

---
+++ day_four.py
@@ -0,0 +1,13 @@
+# Day Four
+
+temperature = int(input("Enter the temperature: "))
+
+if temperature >= 90:
+    print("It is hot.")
+elif temperature >= 70:
+    print("It is warm.")
+elif temperature >= 50:
+    print("It is cool.")
+else: 
+    print("It is cold.")
+
